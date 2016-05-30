e7ef4a72052fc564eafcabd06590f107c245a34e
yzhou <yzhou@buaa.edu.cn>
2016-05-30 23:29:16 +0800

    pod

--- Podfile
+++ Podfile
@@ -8,4 +8,10 @@ target  ‘ZHYFramework’ do
     pod 'MBProgressHUD',            '~>0.9.2'
     pod 'FDFullscreenPopGesture',   '~>1.1'
     pod 'GVUserDefaults',           '~>1.0.1'
+    pod 'SDWebImage',               '~>3.7.3'
+    pod 'DZNEmptyDataSet',          '~>1.7.1'
+    pod 'IQKeyboardManager',        '~>3.3.0'
+    pod 'CRToast',                  '~>0.0.9'
+    pod 'Masonry',                  '~>1.0.0'
+    pod 'SVPullToRefresh',          '~>0.4.1'
 end
--- Pods/Target Support Files/Pods-ZHYFramework/Pods-ZHYFramework-frameworks.sh
+++ Pods/Target Support Files/Pods-ZHYFramework/Pods-ZHYFramework-frameworks.sh
@@ -85,15 +85,27 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/CRToast/CRToast.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/DZNEmptyDataSet/DZNEmptyDataSet.framework"
   install_framework "$BUILT_PRODUCTS_DIR/FDFullscreenPopGesture/FDFullscreenPopGesture.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GVUserDefaults/GVUserDefaults.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/IQKeyboardManager/IQKeyboardManager.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Masonry/Masonry.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SVPullToRefresh/SVPullToRefresh.framework"
   install_framework "$BUILT_PRODUCTS_DIR/YYModel/YYModel.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/CRToast/CRToast.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/DZNEmptyDataSet/DZNEmptyDataSet.framework"
   install_framework "$BUILT_PRODUCTS_DIR/FDFullscreenPopGesture/FDFullscreenPopGesture.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GVUserDefaults/GVUserDefaults.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/IQKeyboardManager/IQKeyboardManager.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Masonry/Masonry.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SVPullToRefresh/SVPullToRefresh.framework"
   install_framework "$BUILT_PRODUCTS_DIR/YYModel/YYModel.framework"
 fi
